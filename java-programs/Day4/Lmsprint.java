2e686bfbb67ba4827462efc225103f8b0696498d
Jayasurya K <jsurya0333@gmail.com>
2025-12-22 17:03:47 +0530

    update var name

--- java-programs/Day4/Lmsprint.java
+++ java-programs/Day4/Lmsprint.java
@@ -3,8 +3,8 @@ package Day4;
 public class Lmsprint {
 
 	public static void main(String[] args) {
-		String names=args[0];
-		System.out.println(names);
+		String namesx=args[0];
+		System.out.println(namesx);
 	}
 
 }
